2be177a675cbc8592992310ad57ea7598acf306b
Javedmv <javedmv777@gmail.com>
2024-04-08 01:29:42 +0530

    todo completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [toDos,setToDos] = useState([]);
+    const [toDo,setToDo] = useState('');
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const day = new Date().getDay()
+
+    return (
+        <div className="app">
+          <div className="mainHeading">
+            <h1>ToDo List</h1>
+          </div>
+          <div className="subHeading">
+            <br />
+            <h2>Whoop, it's {daysOfWeek[day]} 🌝 ☕ </h2>
+          </div>
+          <div className="input">
+            <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+            <i onClick={()=>setToDos([...toDos,{text:toDo,id:Date.now(),status:false}])} className="fas fa-plus"></i>
+          </div>
+          <div className="todos">
+            { toDos.map((list) => {
+                return (
+                  <div key={list.id} className="todo">
+                  <div className="left">
+                    <input value={list.status} onChange={(e) => {
+                      setToDos(toDos.filter(obj => {
+                        if(obj.id === list.id){
+                          obj.status = e.target.checked;                         
+                        }
+                        return obj;
+                      }))
+                    }
+                      } type="checkbox" name="" id="" />
+                    <p>{list.text}</p>
+                  </div>
+                  <div className="right">
+                    <i className="fas fa-times"></i>
+                  </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      );
 }
 
 export default App;
